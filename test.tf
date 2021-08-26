6c209a9860e2ce2397a2077dc7c4149a832756b8
mugioka <okamugi0722@gmail.com>
2021-08-26 16:46:24 +0900

    Add test.tf.

---
+++ test.tf
@@ -0,0 +1,21 @@
+data "aws_s3_bucket" "acme-s3-access-logging" {
+  bucket = "test"
+}
+
+resource "aws_s3_bucket" "not-another-public-one-please" {
+  bucket = "my-public-bucket"
+  acl    = "public-read"
+
+  logging {
+    target_bucket = data.aws_s3_bucket.acme-s3-access-logging.id
+    target_prefix = "my-passing-bucket/logs/"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
