bb35e686c787b0e261f5607af0b42887b4e57521
mugi-o <62197019+mugioka@users.noreply.github.com>
2021-08-30 13:09:38 +0900

    Update s3_buckets.tf

--- s3_buckets.tf
+++ s3_buckets.tf
@@ -1,4 +1,3 @@
-
 data "aws_s3_bucket" "acme-s3-access-logging" {
 
   bucket = var.acme_s3_logging_bucket
@@ -94,4 +93,3 @@ resource "aws_s3_bucket" "not-another-public-one-please" {
     }
   }
 }
-
